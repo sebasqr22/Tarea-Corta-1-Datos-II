af31b26b7afc0cc3d44f97c84744bef77e496ee6
sebasqr22 <sebastianqr.2208@gmail.com>
2021-03-08 09:56:19 -0600

    Se pone un switch en la clase menu

--- menu.cpp
+++ menu.cpp
@@ -14,20 +14,18 @@ class menuInicio{
             cout << "Por favor seleccione una opcion: \n" << "1. Agregar un numero a la lista\n" << "2. ELiminar numero de la lista\n" << "3. Salir\n";
             cout << "Opcion: ";
             cin >> num;
-            if(num == 1){
-                int nuevoNum;
-                cout << "\n" << "Ingrese un numero a agregar a la lista: ";
-                cin >> nuevoNum;
-                cout << endl;
-                listaUtilizar.Insertar(nuevoNum);
-                listaUtilizar.ImprimirLista();
-                return;
-            }
-            else if (num == 2){
+            switch (num){
+                case 1:
+                    int nuevoNum;
+                    cout << "\n" << "Ingrese un numero a agregar a la lista: ";
+                    cin >> nuevoNum;
+                    cout << endl;
+                    listaUtilizar.Insertar(nuevoNum);
+                    listaUtilizar.ImprimirLista();
+                    menuInicio();
 
-            }
-            else{
-
+                default:
+                    cout << "Por favor seleccione alguna opcion existente...\n";
             }
         };
 };
